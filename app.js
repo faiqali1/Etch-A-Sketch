4f3c5f370ef35b0f88227523db3c72ea91d78743
faiqali1 <74228240+faiqali1@users.noreply.github.com>
2021-07-24 12:07:35 +0300

    Only Bar left

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const container = document.getElementById("drawing-board");
 
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-row', rows);
-  container.style.setProperty('--grid-column', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-    cell.addEventListener('mouseenter',function (){
-        cell.style.backgroundColor = 'black';
-    })
-  };
+    container.style.setProperty('--grid-row', rows);
+    container.style.setProperty('--grid-column', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        // cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+        cell.addEventListener('mouseenter',function (){
+            cell.style.backgroundColor = 'black';
+        })
+    };
 };
+
+function reset() {
+    let array = document.getElementsByClassName("grid-item")
+    
+    for (let index = 0; index < array.length; index++) {
+        array[index].style.backgroundColor = 'white';
+        
+    } 
+}
+function changeSize(num) {
+    let array = document.getElementsByClassName("grid-item")
+    const container = document.getElementById("drawing-board");
+    
+    
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    
+    // for (let index = 0; index < array.length; index++) {
+    //     array[index].removeChild;
+    //     console.log("loop");
+    // }
+    makeRows(num,num);
+}
 
 makeRows(16, 16);
